e5c6596400da16e6aa8367b8231e41b7a7ef8a0d
JuanIgnacioTosetti <132720472+JuanIgnacioTosetti@users.noreply.github.com>
2023-06-25 20:04:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function vistaNocturna() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "#90ffe9";
+    document.getElementById("nav").style.backgroundColor = "black";
+    document.getElementById("nav").style.color = "black";
+    document.getElementById("text").style.color = "whitesmoke";
+    document.getElementById("preparacion").style.backgroundColor = "black";
+    document.getElementById("preparacion").style.color = "whitesmoke";
+    document.getElementById("ingredientes").style.color = "whitesmoke";
+    document.getElementById("footer").style.backgroundColor = "black";
+}
+
+
+function mostrarInfo(){
+    alert('Juan Ignacio Tosetti, La plata, Buenos Aires, Argentina')
+}
