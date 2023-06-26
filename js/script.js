44055c5a406753ed3678e33353d28c3f8239622a
JuanIgnacioTosetti <132720472+JuanIgnacioTosetti@users.noreply.github.com>
2023-06-26 17:53:36 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,10 +5,28 @@ function vistaNocturna() {
     document.getElementById("nav").style.backgroundColor = "black";
     document.getElementById("nav").style.color = "black";
     document.getElementById("text").style.color = "whitesmoke";
-    document.getElementById("preparacion").style.backgroundColor = "black";
+    
     document.getElementById("preparacion").style.color = "whitesmoke";
     document.getElementById("ingredientes").style.color = "whitesmoke";
     document.getElementById("footer").style.backgroundColor = "black";
+
+    document.getElementById("imgs").style.backgroundColor ="#254242";
+}
+
+function restaurarEstilos() {
+    
+    document.body.style.backgroundColor = "#f6eddc";
+    document.getElementById("header").style.backgroundColor = "#254242";
+    document.getElementById("header").style.color = "#f6eddc";
+    document.getElementById("nav").style.backgroundColor = "#254242";
+    document.getElementById("nav").style.color = "#f6eddc";
+    document.getElementById("text").style.color = "black";
+    
+    document.getElementById("preparacion").style.color = "black";
+    document.getElementById("ingredientes").style.color = "black";
+    document.getElementById("footer").style.backgroundColor = "#586875";
+
+    document.getElementById("imgs").style.backgroundColor ="#e3e5d7";
 }
 
 
